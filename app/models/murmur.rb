050c3eb5782fed6cbc6271e4f455f66c1f377348
HeChian <hechian@kdanmobile.com>
2012-06-28 23:35:11 +0800

    自動產生亂碼字當generated_url

--- app/models/murmur.rb
+++ app/models/murmur.rb
@@ -1,3 +1,17 @@
 class Murmur < ActiveRecord::Base
   attr_accessible :content, :generated_url
+
+  before_save :generate_url
+
+  private
+  def generate_url
+    self.generated_url = random_string
+  end
+
+  def random_string(string_length = 10)
+    string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    tmp    = ''
+    string_length.times{ tmp << string[rand(string.size)] }
+    tmp
+  end
 end
