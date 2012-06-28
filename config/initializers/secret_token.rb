e3f852aa4a291ef1c639ff7631fcbd4123ec3e40
HeChian <hechian@kdanmobile.com>
2012-06-28 22:40:54 +0800

    First initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MurmurCoder::Application.config.secret_token = 'f1324b7b71aae85d315f21cf5893fa0eb6ca5c393df3da4d9fb47e996604517797e3b96e439b48a6d7426ae43ae6db5c424208f6a974b65d0ee62f03275eff4e'
